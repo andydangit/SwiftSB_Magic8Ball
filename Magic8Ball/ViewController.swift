4d43e8cf74a1626d32b19da4cb2e57630cf525d3
andydangit <andy.dang.it@gmail.com>
2024-10-10 20:57:21 -0500

    Add: insert random array of 8 ball image

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -11,12 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     
+    let totalBallImage = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     
     @IBAction func IBAction(_ sender: Any) {
+        imageView.image = UIImage(named: totalBallImage.randomElement()!)
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(systemName: "questionmark.circle")
     }
 
 
