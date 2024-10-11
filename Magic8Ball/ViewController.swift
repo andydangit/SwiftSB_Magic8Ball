2e6250d617022f5254049e5bba05cf364e2d6eee
andydangit <andy.dang.it@gmail.com>
2024-10-10 20:37:01 -0500

    Add: basic layout design

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBAction func IBAction(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
